819eee8aaf0692fd82bfdd5484c7e4a6ddecde50
anna-jeongah <annajlee28@gmail.com>
2023-03-13 20:46:59 -0600

    Added constructor

--- com.autovend.software/src/com/autovend/software/PrintReceipt.java
+++ com.autovend.software/src/com/autovend/software/PrintReceipt.java
@@ -1,5 +1,15 @@
 package com.autovend.software;
 
+import com.autovend.devices.SelfCheckoutStation;
+
 public class PrintReceipt {
+	
+	private  SelfCheckoutStation scs;
+	
+	public PrintReceipt(SelfCheckoutStation selfcheckoutstation) {
+		scs = selfcheckoutstation;
+		
+	}
+	
 
 }

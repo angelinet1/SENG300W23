4fec89807a2862f72914413d249a97e9dbbd3d13
Tyson-Hartley <127632992+Tyson-Hartley@users.noreply.github.com>
2023-03-17 12:19:29 -0600

    Moved AddItem and AddItemByScanner Into SelfCheckoutMachineLogic

--- com.autovend.software/src/com/autovend/software/SelfCheckoutMachineLogic.java
+++ com.autovend.software/src/com/autovend/software/SelfCheckoutMachineLogic.java
@@ -1,13 +1,87 @@
 package com.autovend.software;
 
-import java.util.ArrayList;
+import com.autovend.Barcode;
+import com.autovend.devices.AbstractDevice;
+import com.autovend.devices.BarcodeScanner;
+import com.autovend.devices.observers.AbstractDeviceObserver;
+import com.autovend.devices.observers.BarcodeScannerObserver;
+import com.autovend.external.ProductDatabases;
+import com.autovend.products.BarcodedProduct;
+import com.autovend.products.Product;
 
-import com.autovend.devices.observers.BillStorageObserver;
+public class SelfCheckoutMachineLogic implements BarcodeScannerObserver {
+	
+	
+	static TransactionReciept currentBill;
+	public static boolean machineLocked = false;
 
-public class SelfCheckoutMachineLogic {
+	
+	public  void addItemPerUnit(Product p, double weight) {
+		
+		if(!machineLocked) {
+		
+		//Assuming it is available		
+		if(currentBill == null) {
+			currentBill = new TransactionReciept(p);
+		} else {
+			currentBill.addProduct(p);
+		}
+		
+		currentBill.addProduct(p);
+		
+		// Update Expected Weight
+		currentBill.augmentExpectedWeight(weight);
+		
+		}
+	}
+	
 	
 
+	private BarcodedProduct getBarcodedProductFromBarcode(Barcode barcode) {
+		BarcodedProduct foundProduct = null;
+		
+		if(ProductDatabases.BARCODED_PRODUCT_DATABASE.containsKey(barcode)){
+			foundProduct = ProductDatabases.BARCODED_PRODUCT_DATABASE.get(barcode);
+				};
+		
+		return foundProduct;
+	}
+
+
+
+	@Override
+	public void reactToEnabledEvent(AbstractDevice<? extends AbstractDeviceObserver> device) {
+		this.machineLocked = false; 
+		
+	}
+
+
+
+	@Override
+	public void reactToDisabledEvent(AbstractDevice<? extends AbstractDeviceObserver> device) {
+		this.machineLocked = true;
+		
+	}
+
+
+
+	@Override
+	public void reactToBarcodeScannedEvent(BarcodeScanner barcodeScanner, Barcode barcode) {
+		BarcodedProduct scannedProduct = getBarcodedProductFromBarcode(barcode);
+		
+		if(scannedProduct != null) {
+		addItemPerUnit(scannedProduct, scannedProduct.getExpectedWeight());
+		}
+		
+	}
 	
+	public static void setMachineLock(boolean newState) {
+		
+	}
+	
+	public static TransactionReciept getCurrentBill() {
+		return currentBill;
+	}
 
 
 }

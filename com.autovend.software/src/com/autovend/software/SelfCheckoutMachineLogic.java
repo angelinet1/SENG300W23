dc21da5a4a3a704d7f4ca99feba84b4962364258
anna-jeongah <annajlee28@gmail.com>
2023-03-18 15:22:50 -0600

    Merged logic class from receiptPrinterTest2 branch to logic class in main.

--- com.autovend.software/src/com/autovend/software/SelfCheckoutMachineLogic.java
+++ com.autovend.software/src/com/autovend/software/SelfCheckoutMachineLogic.java
@@ -1,20 +1,46 @@
-/**
- *  @authors: Angeline Tran (301369846), Tyson Hartley (30117135), Jeongah Lee (30137463), Tyler Nguyen (30158563), Diane Doan (30052326), Nusyba Shifa (30162709)
+
+/*
+ *  @author: Angeline Tran (301369846),
+ *  @author: Tyson Hartley (30117135), 
+ *  @author: Jeongah Lee (30137463), 
+ *  @author: Tyler Nguyen (30158563), 
+ *  @author: Diane Doan (30052326), 
+ *  @author: Nusyba Shifa (30162709)
  */
 
 package com.autovend.software;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+
 import com.autovend.Barcode;
-import com.autovend.devices.BarcodeScanner;
-import com.autovend.devices.EmptyException;
-import com.autovend.devices.OverloadException;
+import com.autovend.devices.BillSlot;
+import com.autovend.devices.BillDispenser;
+import com.autovend.Bill;
+import com.autovend.software.BillSlotObserverStub;
+import com.autovend.software.BillValidatorObserverStub;
+import com.autovend.products.BarcodedProduct;
 import com.autovend.devices.SelfCheckoutStation;
 import com.autovend.external.ProductDatabases;
-import com.autovend.products.BarcodedProduct;
 import com.autovend.products.Product;
 
 public class SelfCheckoutMachineLogic{
 	
+	public BillSlot billSlot; // create bill slot
+	public BillDispenser dispenser; // create bill dispenser
+	public Bill bill; // create a bill
+	public Barcode barcode; // create a barcode
+	public BarcodedProduct item; // create a barcoded product
+	public BigDecimal price; // create local variable price
+	public BigDecimal total; // create local variable total
+	public BigDecimal remainder; // create local variable remainder
+	public BigDecimal change; // create local variable change
+	public BillSlotObserverStub listener_1; // create listener
+	public CashIO cashIO; // create cash i/o
+	public CustomerIO customerIO; // create customer i/o
+	public TransactionReceipt items; // create items from the purchase
+	public boolean billInsertedEvent = false;
+	public boolean billValidEvent = false;
 	
 	TransactionReceipt currentBill;
 	public  boolean machineLocked = false;
@@ -25,7 +51,6 @@ public class SelfCheckoutMachineLogic{
 	public PrintReceipt printReceipt; //This is the controller for printing the receipt
 	public AttendantIO attendant = new AttendantIO(); //Creating an attendantIO that will receive and store calls to attendant
 	public CustomerDisplayIO customerDisplay = new CustomerDisplayIO(); //Creating a display where messages to customers can go
-	
 	
 	
 	/**Codes for reasons the Machine is Locked
@@ -61,8 +86,10 @@ public class SelfCheckoutMachineLogic{
 		return false;
 		
 	}
+
+	
 	/**
-	 * Constuctor for Adding observers to pieces of hardware
+	 * Constructor for Adding observers to pieces of hardware
 	 */
 	public SelfCheckoutMachineLogic(SelfCheckoutStation scStation) {
 		listOfLockCodes = new int[numberOfLockCodes];
@@ -84,6 +111,12 @@ public class SelfCheckoutMachineLogic{
 	}
 
 	
+	/**
+	 * 
+	 * 	Adds an item that is sold by unit to the currentBill, updating the weight and total balance
+	 * @param p: The product being added
+	 * @param weight: the weight of p in grams
+	 */
 	public void addItemPerUnit(Product p, double weight) {
 		
 		if(!machineLocked) {
@@ -95,7 +128,7 @@ public class SelfCheckoutMachineLogic{
 			currentBill.addProduct(p);
 		}
 		
-		currentBill.addProduct(p);
+		currentBill.augmentBillBalance(p.getPrice());
 		
 		// Update Expected Weight
 		currentBill.augmentExpectedWeight(weight);
@@ -106,13 +139,14 @@ public class SelfCheckoutMachineLogic{
 		}
 	}
 	
-	
 
 
-
-
-
-
+	/**
+	 * 	Takes a barecode and returns a barcoded product if it is a valid barcode
+	 * Otherwise returns null
+	 * @param barcode: The barcode of the Barcoded Product being looked for
+	 * @return If the barcode corossponds to one in the database, return that product otherwise return null
+	 */
 	public static BarcodedProduct getBarcodedProductFromBarcode(Barcode barcode) {
 		BarcodedProduct foundProduct = null;
 		
@@ -121,10 +155,13 @@ public class SelfCheckoutMachineLogic{
 				};
 		
 		return foundProduct;
-}
+	}
 	
 
 
+	/**
+	 * Tells the machine to wait until the customer chnages the weight of the scale
+	 */
 	private void askCustomerToPlaceItemGUI() {
 		
 		//Prompt GUI to tell customer
@@ -138,6 +175,10 @@ public class SelfCheckoutMachineLogic{
 
 
 	
+	/**
+	 * Sets the machines lock state to newState. If the machine is unlocked set reason for lock to 0
+	 * @param newState
+	 */
 	public void setMachineLock(boolean newState) {
 		
 		if(newState == false) {
@@ -146,10 +187,21 @@ public class SelfCheckoutMachineLogic{
 		this.machineLocked = newState;
 	}
 	
+	
+	/**
+	 * 
+	 * @return Returns a reference to the current bill the machine is processing
+	 */
 	public  TransactionReceipt getCurrentBill() {
 		return currentBill;
 	}
 
+	/**
+	 * This function is called whenever there is a weightChanged detected by the scale.
+	 * If the machine is currently being locked becuase it expects a change it weight
+	 * It checks if it is what the billExpects the weight to be otherwise it will lock the machine
+	 * @param totalWeightInGrams: the total weight of the scale in grams.
+	 */
 	public void weightChanged(double totalWeightInGrams) {
 		switch(this.reasonForLock) {
 		
@@ -165,6 +217,46 @@ public class SelfCheckoutMachineLogic{
 				
 		}
 		
+	}
+
+	
+	/*
+	 * Main function for pay with cash
+	 */
+	public void payWithCash(){
+	    item = SelfCheckoutMachineLogic.getBarcodedProductFromBarcode(barcode); // get the scanned item
+    	price = item.getPrice(); // get price of item
+    	total = items.augmentBillBalance(price); // get the total purchase value
+    	
+    	remainder = total; // initialize remaining amount to pay
+    	int compare = remainder.compareTo(BigDecimal.ZERO); // local variable to store comparison
+    	while(compare == 1) { // comparison returns 1 if remainder > 0
+    		if(listener_1.getInsertedEvent()) { // if event is true, continue with procedure
+    			if(billValidEvent){
+    				int insertedBill = bill.getValue(); // get value of the inserted bill
+        		    BigDecimal updateBill = BigDecimal.valueOf(insertedBill); // convert bill to BigDecimal type
+        		    remainder = total.subtract(updateBill); // reduces the remaining amount due by value of inserted bill
+        		    customerIO.setAmount(remainder); // update Customer IO with amount
+    			} else {
+    				// Prompt again for another bill because last one was invalid
+    				return;
+    			}
+
+    		}
+    		else {
+    			// Prompt for bill because no bill was inserted
+    			return;
+    		}
+    		// Reset events at end of the loop
+			billValidEvent = false;
+			billInsertedEvent = false;
+    	}
+    	
+    	change = remainder.abs(); // set the change to be an absolute value
+    	cashIO.setChange(change); //set change using cashI/O
+    	
+    	// Change will then be distributed by BillStorage
+    	// Move on to Receipt Printer
 	}
 	
 	/**

e961a940742554be224636c703f369fc1c9c4112
dianedoan <Dianedd419@hotmail.com>
2023-03-17 19:24:34 -0600

    finished testing for getBarcodedProductFromBarcode()

--- com.autovend.software.test/src/com/autovend/software/test/AddItemByScannerTest.java
+++ com.autovend.software.test/src/com/autovend/software/test/AddItemByScannerTest.java
@@ -6,19 +6,55 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.autovend.*;
+import com.autovend.external.ProductDatabases;
+import com.autovend.products.BarcodedProduct;
+import com.autovend.software.*;
+
 public class AddItemByScannerTest {
-
+	
+	static Barcode milk;
+	static Barcode bread;
+	
+	static BarcodedProduct milk_product;
+	static BarcodedProduct bread_product;
+	
+	/**
+	 * Set up before each test.
+	 */
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() {
+		
+		milk = new Barcode(new Numeral[] {Numeral.zero});
+		bread = new Barcode(new Numeral[] {Numeral.one});
+		
+		ProductDatabases.BARCODED_PRODUCT_DATABASE.put(milk, milk_product);
+		ProductDatabases.BARCODED_PRODUCT_DATABASE.put(bread, bread_product);
 	}
 
+	/**
+	 * Tear down after each test.
+	 */
 	@After
-	public void tearDown() throws Exception {
+	public void tearDown() {
+		// not sure what to put here
 	}
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	/**
+	 * Tests when an scanned item is found in the database.
+	 */
+	@Test 
+	public void getProduct() {
+		SelfCheckoutMachineLogic.getBarcodedProductFromBarcode(milk);
 	}
+	
+	/**
+	 * Tests when an scanned item is null.
+	 */
+	@Test 
+	public void getProductNull() {
+		SelfCheckoutMachineLogic.getBarcodedProductFromBarcode(null);
+	}
+	
 
 }

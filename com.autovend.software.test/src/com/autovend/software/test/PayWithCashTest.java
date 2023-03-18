be111de893e71d2ecbaf3b684b9b0092e6b3cc99
nusyb <nusyb@LAPTOP-0HQP2U0T>
2023-03-18 04:05:33 -0600

    Made new test cases

--- com.autovend.software.test/src/com/autovend/software/test/PayWithCashTest.java
+++ com.autovend.software.test/src/com/autovend/software/test/PayWithCashTest.java
@@ -1,10 +1,17 @@
 package com.autovend.software.test;
 
+import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
+import java.util.currency;
 
+import com.autovend.Barcode;
 import com.autovend.devices.BillSlot;
 import com.autovend.software.BillSlotObserverStub;
+import com.autovend.software.CashIO;
+import com.autovend.software.CustomerIO;
+import com.autovend.software.PayWithCash;
+import com.autovend.software.TransactionReceipt;
 
 public class PayWithCashTest {
 
@@ -14,9 +21,18 @@ public class PayWithCashTest {
 	@Before
 	public void setup() {
 		// create listener objects
-	    listener_1 = new BillSlotObserverStub();
-	    listener_2 = new BillSlotObserverStub();
-	    listener_3 = new BillSlotObserverStub();
+		var listener_1 = new BillSlotObserverStub();
+	    var listener_2 = new BillSlotObserverStub();
+	    var listener_3 = new BillSlotObserverStub();
+	  
+	    var billSlot = new BillSlot();
+	    var barcode = new Barcode();
+	    var transactionreciept = new TransactionReceipt();
+	    var cashIO = new CashIO();
+	    var customerIO = new CustomerIO();
+	    
+	    
+
 	    
 	    // register listener 1 and 2
 	    billSlot.register(listener_1);
@@ -25,6 +41,12 @@ public class PayWithCashTest {
 	    // disable and enable BillSlot to register listeners with device
 	    billSlot.disable();
 	    billSlot.enable();	
+	    
+	    //Call main
+	    PayWithCash.main();
+	    //assertion
+	    assertTrue(listener.getInsertedEvent());
+	    
 	}
 	
 	/*

3dadc36f511fa3ed51d6fa9155f96990c46309e0
Tyson-Hartley <127632992+Tyson-Hartley@users.noreply.github.com>
2023-03-18 20:14:09 -0600

    Revert "Made AddItemPerUnit Update Total Properly"
    This reverts commit 369ff46c76eeb5349b089cebe9ae6edf73347e31.

--- com.autovend.software/src/com/autovend/software/SelfCheckoutMachineLogic.java
+++ com.autovend.software/src/com/autovend/software/SelfCheckoutMachineLogic.java
@@ -131,7 +131,7 @@ public class SelfCheckoutMachineLogic{
 			currentBill.addProduct(p);
 		}
 		
-		currentBill.setBillBalance(currentBill.getBillBalance().add(p.getPrice()));
+		currentBill.augmentBillBalance(p.getPrice());
 		
 		// Update Expected Weight
 		currentBill.augmentExpectedWeight(weight);
@@ -293,7 +293,7 @@ public class SelfCheckoutMachineLogic{
     	//Once the payment is finished, call the signalToPrintReceipt method which will 
     	//run through the process of printing the receipt
     	try {
-			 this.signalToPrintReceipt(currentBill);
+			this.signalToPrintReceipt(currentBill);
 		} catch (Exception e) {
 		}
 	}
